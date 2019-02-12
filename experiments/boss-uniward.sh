40a7b6cbecc611c6cf6bb2aea73ae3c59b228bc2
Susan Jeziorowski <susanjeziorowski@gmail.com>
2019-02-12 17:18:50 -0600

    Uniward and WOW use OG boss instead of copies

--- experiments/boss-uniward.sh
+++ experiments/boss-uniward.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-pushd ../
+pushd ../ #project root directory
 
-echo "Copying boss images into s-uniward directory..."
-rsync -ahr --info=progress2 data/raw/boss/cover/* external/s-uniward/S-UNIWARD_linux_make_v10/images_cover/
+#echo "Copying boss images into s-uniward directory..."
+#rsync -ahr --info=progress2 data/raw/boss/cover/* external/s-uniward/S-UNIWARD_linux_make_v10/images_cover/
+#./S-UNIWARD -v -I ../images_cover -O ../images_stego -a 0.4
 
 echo "Running s-uniward stego algorithm..."
-pushd external/s-uniward/S-UNIWARD_linux_make_v10/executable
+#pushd external/s-uniward/S-UNIWARD_linux_make_v10/executable
 
-bash example_default.sh
+mkdir data/cache data/cache/s-uniward
+
+bash external/s-uniward/S-UNIWARD_linux_make_v10/executable/S-UNIWARD -v -I data/raw/boss/cover/ -O data/cache/s-uniward -a 0.4
 
 echo "Done!"
 
