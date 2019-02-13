62d86256ea1369e6ea9cedd6d03fd266bc8a2263
Susan Jeziorowski <susanjeziorowski@gmail.com>
2019-02-13 16:38:41 -0600

    Upload external folder to susies hpc

---
+++ upload_external.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rsync -ahr --info=progress2 external/* sjeziorow42@shell.hpc.tntech.edu:~/stego_external
+
---
+++ upload_hugo_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rsync -ahr --info=progress2 data/raw/boss/stego/* sjeziorow42@shell.hpc.tntech.edu:~/hugo_boss_data
+
+
+
