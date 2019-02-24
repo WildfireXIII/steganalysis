d4b8f2b5e254b1391a0317a6760317d5f28a288c
Nathan Martindale <nathanamartindale@gmail.com>
2019-02-23 20:10:59 -0600

    Randomize partitions

--- src/cnn.py
+++ src/cnn.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
+import random
 
 import sys
 
@@ -127,7 +128,7 @@ std = np.std(x)
 x -= mean
 x *= (1/std)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=random.randint(1,100))
 
 
 #x, y = randomize(x, y)
